ffdc13a5a0e02759aaf10f52e1b094c8a0e13bef
Muhammad Faiz Faqih <93903100+Basara129@users.noreply.github.com>
2023-12-05 17:18:51 +0700

    Add files via upload

---
+++ sticky.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+
+    window.onscroll = function() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    };
+});
